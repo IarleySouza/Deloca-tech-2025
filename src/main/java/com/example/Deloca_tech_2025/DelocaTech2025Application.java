2e9d0eca9b69ad4707735bde63fc2ce594a041ac
Iarley Souza <iarleysouza615@gmail.com>
2025-03-05 21:53:54 -0300

    Initial commit

---
+++ src/main/java/com/example/Deloca_tech_2025/DelocaTech2025Application.java
@@ -0,0 +1,11 @@
+package com.example.Deloca_tech_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelocaTech2025Application {
+	public static void main(String[] args) {
+		SpringApplication.run(DelocaTech2025Application.class, args);
+	}  	
+}
---
+++ src/test/java/com/example/Deloca_tech_2025/DelocaTech2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Deloca_tech_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelocaTech2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
